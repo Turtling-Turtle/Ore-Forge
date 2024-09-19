3546be066afdc47235da6230c258f8c17ebfdda3
NathanUlmen <Ulmennathan@gmail.com>
2024-09-19 00:29:32 -0600

    Improved the way QuestIcon displays quest conditions.

--- core/src/main/java/ore/forge/ButtonType.java
+++ core/src/main/java/ore/forge/ButtonType.java
@@ -1,38 +1,27 @@
 package ore.forge;
 
 public enum ButtonType {
-    CIRCLE_BOLD_128("128xCircleBold", "200,36,64,64", "24,24,24,24"),
-    CIRCLE_EMPTY_128("128xCircleEmpty", "662,36,64,64", "24,24,24,24"),
-    CIRCLE_FULL_128("128xCircleFull", "68,36,64,64", "24,24,24,24"),
-    ROUND_BOLD_128("128xRoundBold", "728,36,64,64", "24,24,24,24"),
-    ROUND_EMPTY_128("128xRoundEmpty", "2,36,64,64", "24,24,24,24"),
-    ROUND_FULL_128("128xRoundFull", "464,36,64,64", "24,24,24,24"),
-    VERY_ROUND_BOLD_128("128xVeryRoundBold", "266,36,64,64", "24,24,24,24"),
-    VERY_ROUND_EMPTY_128("128xVeryRoundEmpty", "332,36,64,64", "24,24,24,24"),
-    VERY_ROUND_FULL_128("128xVeryRoundFull", "134,36,64,64", "24,24,24,24"),
-    CIRCLE_BOLD_64("64xCircleBold", "794,68,32,32", "12,12,12,12"),
-    CIRCLE_EMPTY_64("64xCircleEmpty", "36,2,32,32", "12,12,12,12"),
-    CIRCLE_FULL_64("64xCircleFull", "2,2,32,32", "12,12,12,12");
+    CIRCLE_BOLD_128("128xCircleBold"),
+    CIRCLE_EMPTY_128("128xCircleEmpty"),
+    CIRCLE_FULL_128("128xCircleFull"),
+    ROUND_BOLD_128("128xRoundBold"),
+    ROUND_EMPTY_128("128xRoundEmpty"),
+    ROUND_FULL_128("128xRoundFull"),
+    VERY_ROUND_BOLD_128("128xVeryRoundBold"),
+    VERY_ROUND_EMPTY_128("128xVeryRoundEmpty"),
+    VERY_ROUND_FULL_128("128xVeryRoundFull"),
+    CIRCLE_BOLD_64("64xCircleBold"),
+    CIRCLE_EMPTY_64("64xCircleEmpty"),
+    CIRCLE_FULL_64("64xCircleFull");
 
     private final String name;
-    private final String bounds;
-    private final String padding;
 
-    ButtonType(String name, String bounds, String padding) {
+    ButtonType(String name) {
         this.name = name;
-        this.bounds = bounds;
-        this.padding = padding;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getBounds() {
-        return bounds;
-    }
-
-    public String getPadding() {
-        return padding;
-    }
 }
