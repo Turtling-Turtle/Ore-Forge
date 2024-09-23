f42a9d6ecbb8243f38b6fd1dba883ff3c246e7f0
NathanUlmen <Ulmennathan@gmail.com>
2024-09-23 16:37:25 -0600

    Testing things

--- core/src/main/java/ore/forge/Screens/Widgets/ItemIcon.java
+++ core/src/main/java/ore/forge/Screens/Widgets/ItemIcon.java
@@ -106,6 +106,7 @@ public class ItemIcon extends Table {
     }
 
     public void setProcessor(InventoryMode processor) {
+        this.processor = processor;
         this.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
